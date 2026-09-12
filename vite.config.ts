bf746c74ed5c98328ff7fc77469e87a54f732e8f
Sandeep Raj <info@thermogenprojects.com>
2026-09-12 12:19:41 +0530

    Set root base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/thermogen-projects-website/',
+    base: '/',
     plugins: [
       react(),
       tailwindcss(),
